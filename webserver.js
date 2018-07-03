b68892c1bf6f9ffac4ea36bf077a41d9bf62067c
tizzytzy <Sama.tzy@outlook.com>
2018-07-04 00:07:34 +0800

    作业2

--- webserver.js
+++ webserver.js
@@ -20,7 +20,7 @@ if (system.args.length !== 2){
     service = server.listen(port,function (request,response) {
         var requestUrl = request.post.url;
         response.headers = {
-        'Cache':'no-cache',
+            'Cache':'no-cache',
             'Content-Type':'text/html;charset=utf-8'
         };
         if (requestUrl){
@@ -29,12 +29,14 @@ if (system.args.length !== 2){
                     console.log('FAIL to load the address');
                     return;
                 }
-                body = page.content;
-                //写入文件
-                //fs.write('./' + requestUrl + ".html",body,'w');
-
-                response.write(body);
+                page.switchToFrame(0);
+                page.includeJs(jq,function () {
+                    page.evaluate(playListsCallback);
+                });
+                handlerPlayListDetail();
+                response.write('success');
                 response.close();
+
             });
         }else {
             var body = 'no spider url';
@@ -44,9 +46,110 @@ if (system.args.length !== 2){
     });
 
     if(service){
-        console.log('Web server running on port' + port);
+        console.log('Web server running on port ' + port);
     }else {
-        console.log('Error:Could not create web server listening on port' + port);
+        console.log('Error:Could not create web server listening on port ' + port);
         phantom.exit();
     }
+}
+
+var jq = "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js";
+
+var playLists = [];
+
+page.onError = function (message) {
+    if (message.indexOf('__data__') != -1){
+        var data = message.substr(8);
+        console.log(data);
+        var jsonData = JSON.parse(data);
+        if (jsonData.dataType === 'playLists'){
+            for (var i = 0;i<jsonData.playLists.length;i++){
+                playLists.push(jsonData.playLists[i]);
+            }
+        }else if (jsonData.dataType === 'playListDetail'){
+            var playListDetailFile = './' + jsonData.id + ".json";
+            if (!fs.exists(playListDetailFile)){
+                fs.write(playListDetailFile,data,'w');
+            }
+        }
+    } else {
+        console.log(message);
+    }
+};
+
+
+function playListsCallback() {
+    var items = [];
+    jQuery('#m-pl-container li').each(function () {
+        var target = jQuery(this);
+        var href = 'https://music.163.com/#' + target.find('.u-cover a').attr('href');
+        var item = {
+            href:href,
+            id:target.find('.icon-play').data('res-id')
+        };
+        items.push(item);
+    });
+
+    var playLists = {
+        dataType:'playLists',
+        playLists:items
+    };
+
+    console.error('__data__' + JSON.stringify(playLists,undefined,4));
+}
+
+function detailCallback() {
+    var playList = {
+        dataType:'playListDetail',
+        title:jQuery('.tit h2').text(),
+        id:jQuery('#content-operation').data('rid'),
+        cover:jQuery('.u-cover img').attr('src'),
+        desc:jQuery('#album-desc-dot').html(),
+        songs:[]
+    };
+
+    jQuery('.m-table tbody tr').each(function () {
+        var song = {
+            id:jQuery(this).find('.hd span').data('res-id'),
+            name:jQuery(this).find('b').attr('title'),
+            duration:jQuery(this).find('.u-dur').text(),
+            singer:jQuery(this).find('.text').attr('title'),
+            albumName:jQuery(this).find('.text a').attr('title')
+        };
+        playList.songs.push(song);
+    });
+
+    console.error('__data__' + JSON.stringify(playList,undefined,4))
+}
+
+
+function handlerPlayListDetail() {
+    setTimeout(invokeDetail,0);
+}
+
+function invokeDetail() {
+    if (playLists.length === 0){
+        handlerPlayListDetail();
+        return;
+    }
+    var playListHref = playLists[0].href;
+    var playListDetailFile = './' + playLists[0].id + ".json";
+    playLists = playLists.slice(1);
+    if (fs.exists(playListDetailFile)){
+        handlerPlayListDetail();
+        return;
+    }
+    page.open(playListHref,function (status) {
+        if (status !== 'success'){
+            console.log('FAIL to load the address',page.url);
+            playLists.push(page.url);
+            handlerPlayListDetail();
+            return;
+        }
+        console.log('SUCCESS to load the address',page.url);
+        page.includeJs(jq,function () {
+            page.evaluate(detailCallback);
+            handlerPlayListDetail();
+        });
+    });
 }
